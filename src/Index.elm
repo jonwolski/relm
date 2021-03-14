c18019483aeffe11b6334a3b56872032e33ab7ca
Bender <jmichaelphilips@gmail.com>
2021-03-14 11:08:50 -0400

    feature(1) Create basic login page

---
+++ src/Index.elm
@@ -0,0 +1,21 @@
+module Index exposing (main)
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+
+
+view model =
+    div [ class "jumbotron" ]
+        [ h1 [] [ text "Relm!" ]
+        , p []
+            [ text "Real World Application in Elm -> Relm"
+            , text <|
+                """
+                This is the real world application + playground written in elm
+                """
+            ]
+        ]
+
+
+main =
+    view "dummy model"
