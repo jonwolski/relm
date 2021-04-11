c3b02896236245fee33e5fd155979ada9173c4d7
Joshua Philips <jmichaelphilips@gmail.com>
2021-04-11 17:12:55 -0400

    feat: routing
    * Create main.elm
    
    Remove elm-stuff from git
    
    * Add node_modules to .gitignore
    
    * Update the readme
    
    * Add new folders for front-end and backend work
    
    * 6-routing: Delete Login.elm
    
    * 6-routing move Login functions to SignUp
    
    * Update repo for Elixir Project
    
    * 6-routing Update sign up page to be more inclusive
    
    * Remove elm-stuff from version control
    
    Even though `elm-stuff` was added to the `.gitignore` file,
    the directory was already checked in, so the files remain in version
    control.
    
    This commit removes them from the git index.
    
    Additionally, ignoring `elm-stuff/*` ignores the files in the directory,
    but not the directory itself, so when I build, `elm-stuff` still looks
    like something that should be added (when I do a `git status`).
    
    git-ignoring `elm-stuff` will ignore any file or folder with that name
    (and any files inside that folder).
    
    * Remove vestigial import
    
    This `import Post`... produced a compile error.
    
    * Fix function declaration syntax
    
    Co-authored-by: Jon Wolski <jonwolski@gmail.com>

--- frontend/README.md
+++ frontend/README.md
@@ -3,3 +3,9 @@
 Rolling Implementation of Real World.  Currently implementing frontend in Elm.  Potentially backend in Elixir, but who knows.
 
 https://github.com/gothinkster/realworld
+
+## Development
+
+Currently being developed in elm version 0.19.1
+
+After installing elm, run the command `elm reactor` in the route of the directory.
---
+++ frontend/src/SignUp.elm
@@ -0,0 +1,104 @@
+module SignUp exposing (..)
+
+import Browser
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onClick, onInput)
+import Http
+
+
+
+-- MAIN
+
+
+main =
+    Browser.sandbox { init = init, update = update, view = view }
+
+
+-- USER
+
+type alias SignUp =
+  { email : String
+  , username : String
+  , password : String
+  , passwordAgain : String
+  , submitted : Bool
+  }
+
+init : SignUp
+init = SignUp "" "" "" "" False
+
+
+-- UPDATE
+type Msg
+  = Email String
+  | Username String
+  | Password String
+  | PasswordAgain String
+  | Validate
+
+
+update : Msg -> SignUp -> SignUp
+update msg model =
+  case msg of
+    Email email ->
+      { model | email = email }
+
+    Username username ->
+      { model | username = username }
+
+    Password password ->
+      { model | password = password }
+
+    PasswordAgain password ->
+      { model | passwordAgain = password }
+
+    Validate ->
+      { model | submitted = True }
+
+
+signUp : SignUp -> Cmd Msg
+signUp model =
+  Http.post
+    { url = "http://localhost:6001/"
+    , body = Http.emptyBody
+    , expect = Http.expectJson
+    }
+
+-- VIEW
+view : SignUp -> Html Msg
+view model =
+    div []
+    [ viewInput "text" "Username" model.username Username
+    , viewInput "text" "Email" model.email Email
+    , viewInput "password" "Password" model.password Password
+    , viewInput "password" "Re-enter Password" model.passwordAgain PasswordAgain
+    , viewSubmit "Sign Up" model
+    , viewValidation model.password model.passwordAgain
+    , viewInformation model
+    ]
+
+
+viewInput : String -> String -> String -> (String -> msg) -> Html msg
+viewInput t p v toMsg =
+  input [type_ t, placeholder p, value v, onInput toMsg] []
+
+
+viewSubmit : String -> SignUp -> Html Msg
+viewSubmit title model =
+  button [ onClick Validate ] [text title]
+
+
+viewValidation : String -> String -> Html msg
+viewValidation password passwordAgain =
+  if password == passwordAgain then
+    div [ style "color" " green"] [ text "OK"]
+  else
+    div [ style "color" "red"] [ text "passwords do not match"]
+
+viewInformation : SignUp -> Html msg
+viewInformation model =
+  if model.submitted == True then
+    div [ style "color" "blue" ] [text model.email, text model.username]
+  else
+    div [style "color" "red"] [text "Please Sign Up :)"]
